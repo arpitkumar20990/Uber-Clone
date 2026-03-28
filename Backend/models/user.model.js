f5096cdb86ff95c93c4e1d94d5c1a6a84c1f2ee4
arpitkumar20990 <arpitchaudharychhillar777@gmail.com>
2026-03-28 21:06:57 +0530

    work of 28 march

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     fullname : {
@@ -16,16 +18,32 @@ const userSchema = new mongoose.Schema({
         type: String,
         required : true,
         unique : true,
-        minlenght :[5, 'email mustr be at least 5 character long']
+        minlength :[5, 'email mustr be at least 5 character long']
     },
     password : {
         type : String, 
-        required : true
+        required : true,
+        select : false
     },
-
     socketId : {
         type: String
     }
 })
 
-// last ended at 15:22 mins
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(password){
+    const isMatch = await bcrypt.compare(password, this.password);
+    return isMatch;
+}
+
+userSchema.statics.hashPassword = async function(password){
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return hashedPassword;
+}
+
+const userModel = mongoose.model('user', userSchema);
+module.exports = userModel;
---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {body} = require('express-validator')
+
+
+
+
+
+
+module.exports = router;
+
+// 21:46 mins
