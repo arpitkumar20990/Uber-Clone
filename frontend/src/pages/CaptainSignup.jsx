78dc77f5b514ef7f47803884c6fe8b07051fb8e5
arpitkumar20990 <arpitchaudharychhillar777@gmail.com>
2026-04-12 08:54:43 +0530

    signup and login page

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const CaptainSignup = () => {
+
+     const [email, setEmail] = useState('')
+      const [password, setPassword] = useState('')
+      const [firstName, setFirstName] = useState('')
+      const [lastName, setLastName] = useState('')
+      const [userData, setUserData] = useState({})
+    
+      const submitHandler = (e) =>{
+        e.preventDefault()
+        setUserData({
+          userName:{
+            firstName:firstName,
+            lastName:lastName
+          },
+          email:email,
+          password:password
+        })
+    
+        setEmail('')
+        setFirstName('')
+        setLastName('')
+        setPassword('')
+      }
+
+
   return (
-    <div>CaptainSignup</div>
-  )
+    <div className='p-7 h-screen justify-between flex flex-col'>
+         <div>
+            <img className='w-16 mb-10' src="https://logos-world.net/wp-content/uploads/2020/05/Uber-Logo.png" alt="" />
+        <form  onSubmit={(e)=>{
+          submitHandler(e)
+          }}>
+                 <h3 className='text-base font-medium mb-2'>What's our Captain's name</h3>
+                 <div className='flex gap-4 mb-5'>
+                  <input 
+                 required
+                 className='bg-[#eeeeee]  rounded px-4 py-2 outline-none w-full text-base placeholder:text-sm' 
+                 type="text"
+                 placeholder='Firstname'
+                 value={firstName}
+                 onChange={(e)=>{
+                  setFirstName(e.target.value)
+                 }}
+                 />
+
+                 <input 
+                 required
+                 className='bg-[#eeeeee]  rounded px-4 py-2 outline-none w-full text-base placeholder:text-sm' 
+                 type="text"
+                 placeholder='Lastname'
+                 value={lastName}
+                 onChange={(e)=>{
+                  setLastName(e.target.value)
+                 }}
+                 />
+
+                 </div>
+                 
+                 <h3 className='text-base font-medium mb-2'>What's our Captain's email</h3>
+                 <input 
+                 required
+                 className='bg-[#eeeeee] mb-5 rounded px-4 py-2 outline-none w-full text-base placeholder:text-sm' 
+                 type="email"
+                 placeholder='email@example.com'
+                 value={email}
+                 onChange={(e)=>{
+                  setEmail(e.target.value)
+                 }}
+                 />
+
+                 <h3 className='text-base font-medium mb-2' >Enter your password</h3>
+                 <input
+                 className='bg-[#eeeeee] mb-5 rounded px-4 py-2 outline-none w-full text-base placeholder:text-sm' 
+                 required 
+                 type="password" 
+                 placeholder='Password'
+                 value={password}
+                 onChange={(e)=>{
+                  setPassword(e.target.value)
+                 }}
+                 />
+                 <button className='bg-[#111] text-white mb-3 rounded px-4 py-2  w-full text-base'>Login</button>
+
+                
+        </form> 
+        <p className='text-center'>Already have a account? <Link to='/captain-login' className='text-blue-600'>Login here</Link></p>
+         </div>
+
+         <div>
+            <p className='text-[10px] text-gray-600'>By continuing, you agree to calls, including by autodialer, WhatsApp, or texts from Uber and its affiliates.</p>
+         </div>
+    </div>
+  ) 
 }
 
 export default CaptainSignup
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div>
-        <div className='bg-cover bg-bottom bg-[url(https://images.unsplash.com/photo-1674395364266-76316bbc82fa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dHJhZmljJTIwbGlnaHR8ZW58MHx8MHx8fDA%3D)] h-screen w-ful pt-8 flex justify-between flex-col bg-red-400'>
+        <div className='bg-cover bg-bottom bg-[url(https://images.unsplash.com/photo-1674395364266-76316bbc82fa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dHJhZmljJTIwbGlnaHR8ZW58MHx8MHx8fDA%3D)] h-screen w-ful pt-8 flex justify-between flex-col'>
             <img className='w-18 ml-8' src="https://logos-world.net/wp-content/uploads/2020/05/Uber-Logo.png" alt="" />
             <div className='bg-white pb-7 p-4'>
                 <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
